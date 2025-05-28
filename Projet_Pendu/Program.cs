fa711a18bccd03cbda28f28b316361759529ef90
Thibault <Thibijo@gmail.com>
2025-05-28 14:07:47 +0200

    Structure de base du projet + lecture des mots
    - Initialisation du projet C#
    
    - Ajout de la fonction ChargerListeMots()
    
    - Ajout de la fonction ObtenirMotADeviner()

--- Projet_Pendu/Program.cs
+++ Projet_Pendu/Program.cs
@@ -2,9 +2,42 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            string cheminFichier = "EST-M3Prog-ED12_JeuDuPendu_ListeMots.txt";
+            List<string> listeMots;
+
+            try
+            {
+                listeMots = ChargerListeMots(cheminFichier);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine($"Le fichier {Path.GetFullPath(cheminFichier)} est introuvable.");
+                return;
+            }
+
+            string motADeviner = ObtenirMotADeviner(listeMots);
+            Console.WriteLine($"Mot à deviner (DEBUG) : {motADeviner}"); // À retirer plus tard
+        }
+
+        /// <summary>
+        /// Charge tous les mots depuis un fichier texte.
+        /// </summary>
+        static List<string> ChargerListeMots(string nomFichier)
+        {
+            return File.ReadLines(nomFichier).ToList();
+        }
+
+        /// <summary>
+        /// Sélectionne un mot aléatoirement dans une liste.
+        /// </summary>
+        static string ObtenirMotADeviner(List<string> mots)
+        {
+            Random random = new Random();
+            int index = random.Next(mots.Count);
+            return mots[index];
         }
     }
 }
+
